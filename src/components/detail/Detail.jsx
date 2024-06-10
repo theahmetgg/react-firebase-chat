e40e8fbeae3671277582472fd4927ac9931c83ec
theahmetgg <ahmet1998gulmez.com>
2024-06-10 18:41:02 +0300

    Chat Detail Component Design

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,6 +1,83 @@
 import "./detail.css";
 const Detail = () => {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Can Ali</h2>
+        <p>Lorem ipsum dolor sit, amet consecte</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/23733220/pexels-photo-23733220/free-photo-of-isik-parlak-acik-hafif.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/23733220/pexels-photo-23733220/free-photo-of-isik-parlak-acik-hafif.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/23733220/pexels-photo-23733220/free-photo-of-isik-parlak-acik-hafif.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/23733220/pexels-photo-23733220/free-photo-of-isik-parlak-acik-hafif.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
