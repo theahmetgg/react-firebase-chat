b06b3a0a44d6d56028da542a31f27992a9b26b47
theahmetgg <ahmet1998gulmez.com>
2024-06-11 16:59:43 +0300

    React Auto-Scroll using useRef Hook

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -75,6 +75,7 @@ const Detail = () => {
           </div>
         </div>
         <button>Block User</button>
+        <button className="logout">Block User</button>
       </div>
     </div>
   );
