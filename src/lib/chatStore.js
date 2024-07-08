6576c8ed837d544f8b73415e0d3d7c4c89a92874
theahmetgg <ahmet1998gulmez@gmail.com>
2024-07-08 14:14:58 +0300

    add reset action

--- src/lib/chatStore.js
+++ src/lib/chatStore.js
@@ -9,8 +9,7 @@ export const useChatStore = create((set) => ({
   changeChat: (chatId, user) => {
     const currentUser = useUserStore.getState().currentUser;
 
-    //CHECK IF CURRENT USER IS BLOCKED
-
+    // CHECK IF CURRENT USER IS BLOCKED
     if (user.blocked.includes(currentUser.id)) {
       return set({
         chatId,
@@ -20,7 +19,7 @@ export const useChatStore = create((set) => ({
       });
     }
 
-    //CHECK IF RECEIVER USER IS BLOCKED
+    // CHECK IF RECEIVER IS BLOCKED
     else if (currentUser.blocked.includes(user.id)) {
       return set({
         chatId,
@@ -37,7 +36,16 @@ export const useChatStore = create((set) => ({
       });
     }
   },
+
   changeBlock: () => {
     set((state) => ({ ...state, isReceiverBlocked: !state.isReceiverBlocked }));
+  },
+  resetChat: () => {
+    set({
+      chatId: null,
+      user: null,
+      isCurrentUserBlocked: false,
+      isReceiverBlocked: false,
+    });
   },
 }));
